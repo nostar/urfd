3b3f65b20ecb3ff2d2617cbaa008065ee262df1b
Dave Behnke <916775+dbehnke@users.noreply.github.com>
2025-12-27 14:53:11 -0500

    fix(m17): allow numeric callsigns/IDs (fixes Unknown M17 packet for DroidStar/M17-259)

--- reflector/Callsign.cpp
+++ reflector/Callsign.cpp
@@ -150,7 +150,7 @@ bool CCallsign::IsValid(void) const
 			iNum++;
 		}
 	}
-	valid = valid && (iNum < 3);
+	// valid = valid && (iNum < 3); // Allow numeric callsigns (e.g. M17, DMR IDs)
 	// all remaining char are letter, number or space
 	for ( ; i < CALLSIGN_LEN; i++)
 	{
